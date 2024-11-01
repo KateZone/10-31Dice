3692ca884c37c4c8109eeebb0109b0f3c14bae20
KateZone <134741223+KateZone@users.noreply.github.com>
2024-11-01 14:44:28 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
-var randomImageSource = "images/"+ randomDiceImage;
-
-var image1 = document.querySelectorAll("img")[0];
 
 
-image1.setAttribute("src", randomImageSource);
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomNumber1 = Math.floor(Math.random()*6)+1; //randomly produce number from 1-6;
+var randomNumber2 = Math.floor(Math.random()*6)+1; //randomly produce number from 1-6;
 
-var randomImageSource2 ="images/dice"+ randomNumber2 + ".png";
+var x = "./images/dice"+randomNumber1+".png";
+var y = "./images/dice"+randomNumber2+".png";
 
-document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+var img1 = document.querySelectorAll("img")[0];   
+img1.setAttribute("src",x);
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML="🚩  Player 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = " Player 2 Wins! 🚩";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw!";
-}
+var img2= document.querySelectorAll("img")[1];
+img2.setAttribute("src",y);
+
+// change the title base on the dice number; 
+    if (randomNumber1 > randomNumber2){
+        document.querySelector("h1");  // find the position of the conten you need to change which is the title"refresh me".
+        var newTitle = document.querySelector("h1");
+        newTitle.innerHTML = "🍄Player 1 Win !";
+    }
+    else if (randomNumber1 < randomNumber2){
+        document.querySelector("h1");  // find the position of the conten you need to change which is the title"refresh me".
+        var newTitle = document.querySelector("h1");
+        newTitle.innerHTML = "Player 2 Win🍄 !";
+    }
+    else if (randomNumber1 = randomNumber2){
+        document.querySelector("h1");  // find the position of the conten you need to change which is the title"refresh me".
+        var newTitle = document.querySelector("h1");
+        newTitle.innerHTML = "🍄Draw🍄";
+    }
+    
